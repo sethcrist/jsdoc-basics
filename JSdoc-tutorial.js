64e7c0fa28b7d2b7665bedfdb55c0141db1ce51a
Seth Crist <sethcrist50@gmail.com>
2024-01-17 15:11:16 -0600

    added a class

--- JSdoc-tutorial.js
+++ JSdoc-tutorial.js
@@ -17,3 +17,44 @@ function createPerson(name, age, isDeveloper) {
         isDeveloper: isDeveloper
     }
 }
+//-----------------------------------------------------------------------
+class Person {
+    /**
+     * creates a person
+     *
+     * @param name
+     * @param age
+     * @param isDeveloper
+     */
+    constructor(name, age, isDeveloper) {
+        this.name = name;
+        this.age = age;
+        this.isDeveloper = isDeveloper;
+    }
+}
+//---------------------------------------------------------------------
+/**
+ * Outputs details about a person
+ *
+ * @param {Person} person
+ */
+
+function printDetails(person) {
+    console.log(`${person.name} is ${person.age} years old and is ${person.isDeveloper}`);
+}
+//-------------------------------------------------------------------
+/**
+ * Gives you numbers
+ *
+ * @returns {number[]}
+ */
+function getNumbers() {
+    return [10, 15, 20, 30]
+}
+
+const numbers = getNumbers();
+
+for (const n of numbers) {
+    console.log(n)
+}
+//-------------------------------------------------------------------
